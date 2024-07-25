15de395e6221cb68319d7d8535c93048c528b3bd
ahmedjamalz <ajamalzayed@gmail.com>
2024-07-25 19:42:36 +0300

    finish custom appBar

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,26 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/views/widgets/custom_search_icon.dart';
+import 'package:notes_app/views/widgets/custom_appBar.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+    return const Padding(
+      padding: EdgeInsets.all(8.0),
       child: Column(
         children: [
           SizedBox(
             height: 30,
           ),
-          Row(children: [
-            Text(
-              'Notes',
-              style: TextStyle(fontSize: 30),
-            ),
-            Spacer(),
-            CustomSearchIcon(),
-          ])
+          CustomAppBar()
         ],
       ),
     );
