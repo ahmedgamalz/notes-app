54c24189536aaf194631d37278a69049d0e0dbb2
ahmedjamalz <ajamalzayed@gmail.com>
2024-07-25 21:56:12 +0300

    create add note button sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -11,7 +12,13 @@ class NotesView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Color(0xff50EDD8),
         shape: CircleBorder(),
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return AddNoteButtonSheet();
+              });
+        },
         child: const Icon(
           Icons.add,
           color: Colors.black,
@@ -19,5 +26,49 @@ class NotesView extends StatelessWidget {
       ),
       body: const NotesViewBody(),
     );
+  }
+}
+
+class AddNoteButtonSheet extends StatelessWidget {
+  const AddNoteButtonSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 32,
+        ),
+        CustomTextField()
+      ],
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: TextField(
+        decoration: InputDecoration(
+            hintText: 'Title',
+            hintStyle: TextStyle(color: kPrimaryColor),
+            border: BuildBorder(),
+            enabledBorder: BuildBorder(),
+            focusedBorder: BuildBorder(kPrimaryColor)),
+        cursorColor: kPrimaryColor,
+      ),
+    );
+  }
+
+  OutlineInputBorder BuildBorder([color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16),
+        borderSide: BorderSide(color: color ?? Colors.white));
   }
 }
